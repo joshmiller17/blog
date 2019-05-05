3e3e3d0b97b45cffba6e29d9970a7fa2a0cfbc3c
Josh Miller <miller.josh@husky.neu.edu>
2019-05-05 19:26:02 -0400

    fix html extensions

---
+++ sync.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+aws s3 sync . s3://joshaaronmiller.com --exclude ".git*"
+htmls=("about" "blog" "poetry" "portfolio" "research" "credits")
+for i in "${htmls[@]}"
+do
+  aws s3 mv s3://joshaaronmiller.com/$i.html s3://joshaaronmiller.com/$i
+done
