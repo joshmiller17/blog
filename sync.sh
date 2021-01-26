dc8b1d9029481ed3e2b1da507817cbe7529857e0
Josh Miller <miller.josh@husky.neu.edu>
2021-01-26 14:55:14 -0500

    Update

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 aws s3 sync . s3://joshaaronmiller.com --exclude ".git*"
-htmls=("about" "blog" "poetry" "portfolio" "research" "credits" "allworks")
+htmls=("about" "tutorials" "blog" "poetry" "portfolio" "research" "credits" "allworks")
 for i in "${htmls[@]}"
 do
   aws s3 mv s3://joshaaronmiller.com/$i.html s3://joshaaronmiller.com/$i
