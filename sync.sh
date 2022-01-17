53f2c58ad3fbc9b7654e59d21a54090fcc6895ee
Josh Aaron Miller <miller.josh@husky.neu.edu>
2022-01-17 17:23:25 -0500

    Fix recipe and sync script

--- sync.sh
+++ sync.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-aws s3 sync . s3://joshaaronmiller.com --exclude ".git*"
-htmls=("about" "tutorials" "blog" "poetry" "portfolio" "research" "credits" "allworks")
-for i in "${htmls[@]}"
-do
-  aws s3 mv s3://joshaaronmiller.com/$i.html s3://joshaaronmiller.com/$i
-done
+htmls=("about" "tutorials" "blog" "poetry" "portfolio" "research" "credits" "allworks" "cta" "whist" "recipes/hotblood")
+if [ -z "$1" ]
+	then
+		aws s3 sync . s3://joshaaronmiller.com --exclude ".git*"
+		for i in "${htmls[@]}"
+		do
+		  aws s3 mv s3://joshaaronmiller.com/$i.html s3://joshaaronmiller.com/$i
+		done
+else
+	aws s3 cp $1.html s3://joshaaronmiller.com/$1.html
+	aws s3 mv s3://joshaaronmiller.com/$1.html s3://joshaaronmiller.com/$1
+fi
+
