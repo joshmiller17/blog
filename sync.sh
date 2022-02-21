fd2f1ca29f430c5e3c7a6c5772da404c93ecc365
Josh Aaron Miller <miller.josh@husky.neu.edu>
2022-02-21 11:37:44 -0500

    Syntax fix

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 htmls=("about" "tutorials" "blog" "poetry" "portfolio" "research" "credits" "allworks" "cta" "whist" "recipes/hotblood" "recipes/butter-bread" "player-groups")
-if [ -z "$1" ]
+if [ -z "$1" ];
 	then
 		aws s3 sync . s3://joshaaronmiller.com --exclude ".git*"
 		for i in "${htmls[@]}"
@@ -9,7 +9,7 @@ if [ -z "$1" ]
 		done
 else
 	aws s3 cp $1.html s3://joshaaronmiller.com/$1.html
-	if ["index" -ne "$1"]
+	if [[ "index" -ne $1 ]];
 	then
 	aws s3 mv s3://joshaaronmiller.com/$1.html s3://joshaaronmiller.com/$1
 	fi
