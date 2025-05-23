e1520590dd8e8057246cfd9f5a90c357a33b5d0b
Josh Miller <miller.josh@northeastern.edu>
2025-05-23 19:27:15 -0400

    Carousel portfolio

---
+++ js/projects.js
@@ -0,0 +1,77 @@
+const projects = [
+    {
+    title: "World of Warcraft",
+    description: `As a content designer on World of Wacraft, I've contributed to multiple features. Here are a few that I'm proud of:
+        <ul>
+        
+        <li>[11.???] Legion Remix feature owner (more details coming soon!)
+        </li><li>[11.0.5] 20th Anniversary Celebration event
+            <ul>
+            <li>Classic Timewalking feature owner
+            </li><li>Dungeon tuning, bug fixes, and modernization
+            </li><li>Feature owner of the Guest Relations secret quests
+            </li><li>Designed and implemented Ratts’ Revenge, a challenging series of secret puzzles
+            </li><li>Blackrock Depths raid development
+            </li><li>“trash” fights, mini-bosses, and flavor spawning
+            </li>
+            </ul>
+        </li><li>[10.2.7] Mists of Pandaria: Remix
+            <ul>
+             <li>Introductory experience (quests, NPCs, hub, and spawns)
+            </li><li>Infinite Bazaars and vendor NPCs
+            </li><li>Repeatable content, consumable items, and additional development
+            </li>
+            </ul>
+        </li><li>[10.2.5] Love is in the Air 2024 holiday update
+        </li><li>[10.2] Winter Veil 2023 gift toys
+        </li><li>[10.1.5 - 10.2.5] Trading Post activities
+        </li><li>[10.0] Artisan’s Consortium quests / NPCs and other Dragonflight profession content
+        </li>
+        
+        </ul>`,
+    image: "assets/mop-remix.png",
+    imageAlt: "Screenshot of a Pandaren monk from World of Warcraft"
+  },
+   {
+    title: "iNaturalist for Unity (2022)",
+    description: `A third-party integration tool that enables you to put real nature sounds and images into your game or make citizen science contributions in your game through iNaturalist. Get it for free <a href="https://assetstore.unity.com/packages/tools/integration/inaturalist-for-unity-208753">here</a>.`,
+    image: "assets/inat.png",
+    imageAlt: "Screenshot of the iNaturalist for Unity demo. You can search for pictures by various features."
+  },
+  {
+    title: "Dan Power's Puzzle Dungeon (2022)",
+    description: `Help me figure out what happened to my friend Dan in Dan Power's Puzzle Dungeon, a game about unreliable narrators, unhelpful guides, and intolerance in games. Check it out <a href="https://joshaaronmiller.itch.io/puzzle-dungeon">here</a>.`,
+    image: "assets/dppd.png",
+    imageAlt: "Screenshot of Dan Power's Puzzle Dungeon: a game that looks like Rogue; on the right of the screen the in-game manual talks about the current puzzle."
+  },
+    {
+    title: "Balance This Game! (2021)",
+    description: `A whimsical, strategic sim about balancing an MMORPG as a summer intern. Can you make your players happy, or will your game flop? Learn more about the game and play for free <a href="https://joshaaronmiller.itch.io/balance-this-game">here</a>.
+		Or read about how I made it in 6 weeks and what I learned in the <a href="https://joshaaronmiller.itch.io/balance-this-game/devlog/309732/postmortem-balance-this-game">postmortem</a>`,
+    embed: `<iframe width="500" height="375" style="float:left" src="https://www.youtube.com/embed/5RAQbvbsR9I"> </iframe>`
+  },
+    {
+    title: "Another Coffee (2021)",
+    description: `This 10 minute interactive fiction was made for the 2020 Global Game Jam "Repair." Our team's take on the theme was a game about repairing an estranged relationship. After the jam, I spent the next year (on and off) polishing the game and released it during the following year's jam. Learn more about the game and play for free <a href="https://joshaaronmiller.itch.io/another-coffee">here</a>.`,
+    image: "assets/anothercoffee.png",
+    imageAlt: "Screenshot of Another Coffee game: two people sit awkwardly in a coffee shop."
+  },
+   {
+    title: "Descent Into Madness (2016/2020)",
+    description: `This one-shot tabletop RPG system was made for a friend who was exploring the life of H.P. Lovecraft for her senior thesis. This Lovecraftian horror game is designed to encourage the party to split up, isolate each other, cast away their only strength, and descend into an isolated tragedy trying to comprehend the cosmic chaos that is an uncontrollable universe. It makes use of a Jenga tower as the primary mechanic (coincidentally, before I'd learned of Dread) as well as a withering limited pool of card resources and a timing dynamic that gives you an increased sense of panic when trying to do something your character hasn't practiced. Published in 2020 in the Indiepocalypse anthology of indie games, volume 1. Available <a href="https://pizzapranks.itch.io/indiepocalypse-1">here</a>.`,
+    image: "assets/descentintomadness.jpg",
+    imageAlt: "Descent Into Madness cover"
+  },
+     {
+    title: "Vennt (2015-2024)",
+    description: `A steampunk fantasy tabletop RPG about teamwork, discovery, and choice. Despite being rules-heavy, Vennt is designed as an exercise in onboarding and unfolding play to ensure that beginner players with no RPG experience can pick up and play. Rules used to be online at <a href="https://www.vennt.fandom.com/">https://www.vennt.fandom.com/</a>, but it seems like you’ll now need a time machine.`,
+    image: "assets/prokawa_steampunk_wizard_rosalie_lenora_datchery.jpg",
+    imageAlt: "A steampunk wizard, art by Prokawa."
+  },
+    {
+    title: "Mino (2019)",
+    description: `Mino is a cooperative strategy board game about amino acids. In this game, players match amino acids to their properties on a grid in order to form hydrogen bonds and make a protein before it denatures. Mino is still in development (on hiatus), but I'm particularly proud of the art style it has so far. Check out how I provided art direction in the <a href="https://docs.google.com/document/d/1CsSLMFga8AXva5PvLnYxVH6M0mJxqTlXW_iPfia8hoo/edit?usp=sharing">style guide / art bible</a> I wrote.`,
+    image: "assets/mino_banner.png",
+    imageAlt: "Expanded logo of Mino showing amino acids as cartoon characters."
+  }
+];
